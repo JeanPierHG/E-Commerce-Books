d53def394f7ad8c498a918448c843fd416bcc0b5
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2022-06-28 09:26:16 -0500

    fix

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -1,5 +1,6 @@
 const { Router } = require('express')
 const router = Router()
+const Books = require('../model/Books')
 const Users = require('../model/Users')
 
 router.get('/', async (req, res) => {
@@ -19,7 +20,7 @@ router.get('/', async (req, res) => {
 
 router.post('/addUser', async (req, res) => {
   const { nickname, name, email, picture, phone, address } = req.body
-
+  console.log(req.body)
   try {
     const isExistUser = await Users.findOne({ email })
     if (isExistUser) throw new Error('User is ready exists')
@@ -57,4 +58,50 @@ router.post('/setAdmin/:id', async (req, res) => {
   }
 })
 
+router.post('/banned/:id', async (req, res) => {
+  const { id } = req.params
+  try {
+    const user = await Users.findByIdAndUpdate(id, { isBanned: true })
+    user.save()
+    res.send('The user is banned!')
+  } catch (error) {
+    res.send(error.message)
+  }
+})
+
+router.post('/addDesiredBooks/:idBook/:idUser', async (req, res) => {
+  const { idBook, idUser } = req.params
+  try {
+    const book = await Books.findById(idBook)
+    const user = await Users.findById(idUser)
+
+    user.favouritesBooks.push(book._id)
+    await user.save()
+
+    res.send('Book successfully added')
+  } catch (error) {
+    res.send(error.message)
+  }
+})
+
+router.post('/deleteDesiredBooks/:idBook/:idUser', async (req, res) => {
+  const { idBook, idUser } = req.params
+  try {
+    const book = await Books.findById(idBook)
+    const user = await Users.findById(idUser)
+
+    user.favouritesBooks = user.favouritesBooks.filter(
+      (b) => b._id !== book._id
+    )
+    await user.save()
+
+    res.send('Book successfully deleted')
+  } catch (error) {
+    res.send(error.message)
+  }
+})
+
+router.post('/comment/:idBook/:idUser', async (req, res) => {})
+
+router.post('')
 module.exports = router
