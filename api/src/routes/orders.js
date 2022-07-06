52c287787c248b5112c0b99b7a925f0ce9a1ad44
Pedro <spagnuolopedro@hotmail.com>
2022-07-06 14:23:48 -0300

    guardado

--- api/src/routes/orders.js
+++ api/src/routes/orders.js
@@ -5,11 +5,17 @@ const router = Router();
 
 router.get("/getAllOrders", async (req, res) => {
   try {
-    const allOrden = await Orders.find({}).populate('usuario')
+    const allOrden = await Orders.find({}).populate("usuario");
     return res.json(allOrden);
   } catch (error) {
     console.log("FALLO EN LAS ORDENES", error);
   }
+});
+
+router.get("/getAllOrdersByUser/:userId", async (req, res) => {
+  const { userId } = req.params;
+  const orders = await Orders.find({ userId: userId }).populate("books");
+  return res.status(200).json(orders);
 });
 
 module.exports = router;
