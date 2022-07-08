9754808e4954d72b5188b7aa2e6202a05217c935
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2022-07-08 12:49:02 -0500

    sigo probando

--- client/src/components/Mercadopago.jsx
+++ client/src/components/Mercadopago.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
 
 function Mercadopago() {
-  const info = async () => {
-    const data = await fetch(
-      'http://ecommercehenryx.herokuapp.com/mercadopago/success'
-    )
-      .then((res) => res.json())
-      .then((data) => data)
-
-    return data
-  }
-
-  console.log(info())
+  fetch('http://ecommercehenryx.herokuapp.com/mercadopago/success')
+    .then((res) => res.json())
+    .then((data) => console.log(data))
 
   console.log('USE PARAMS', data)
   return <div>Hola</div>
