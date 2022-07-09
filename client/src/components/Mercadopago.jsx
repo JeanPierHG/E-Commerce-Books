39702b9d6583b53d5d784e598cffa7a0c4749faf
Jean Pierre Huaman Gomez <jeanpier.dev@outlook.com>
2022-07-08 21:11:01 -0500

    prueba mercado pago

--- client/src/components/Mercadopago.jsx
+++ client/src/components/Mercadopago.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Mercadopago() {
-  const handleClick = async () => {
-    fetch('https://ecommercehenryx.herokuapp.com/mercadopago/success')
-      .then((res) => res.json())
-      .then((data) => console.log(data))
+  const [data, setData] = useState({})
+
+  const handleClick = () => {
+    fetch('https://ecommercehenryx.herokuapp.com/mercadopago/success').then(
+      (res) => res.json().then((data) => setData(data))
+    )
   }
+
+  console.log(data)
 
   return (
     <div>
       <button onClick={handleClick}>Presionar</button>
+      <br />
+      <p>{data.fecha}</p>
+      <p>{data.isHidden}</p>
+      <p>{data.status}</p>
+      <p>{data.produt[0]}</p>
     </div>
   )
 }
